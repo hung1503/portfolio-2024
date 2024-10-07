5dd05e94b0d54294c5fb7508414980344062d458
hung1503 <hungnguyen150300@gmail.com>
2024-10-08 02:55:44 +0300

    Adding work page to portfolio

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,178 @@
-const Work = () => {
-  return (
-    <div>work page</div>
-  )
-}
+"use client";
 
-export default Work
+import { easeIn, motion } from "framer-motion";
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import WorkSliderBtns from "@/components/WorkSliderBtns";
+
+const projects = [
+  {
+    num: "01",
+    category: "Frontend",
+    title: "Netflix Clone",
+    description:
+      "This project was a Netflix client clone built using React, Firebase, and styled-components.",
+    stack: [
+      { name: "React" },
+      { name: "Firebase" },
+      { name: "styled-components" },
+    ],
+    image: "/assets/work/netflix-clone.png",
+    github: "https://github.com/hung1503/netflix-clone",
+  },
+  {
+    num: "02",
+    category: "Frontend",
+    title: "E-commerce website",
+    description:
+      "A basic e-commerce website is built with React, Redux toolkit, TypeScript, SASS, React Router.",
+    stack: [
+      { name: "React" },
+      { name: "Redux toolkit" },
+      { name: "TypeScript" },
+      { name: "SASS" },
+      { name: "React Router" },
+    ],
+    image: "/assets/work/ecommerce.png",
+    github: "https://github.com/hung1503/ecommerce-website",
+  },
+  {
+    num: "03",
+    category: "Fullstack",
+    title: "Dailytrack",
+    description:
+      "Thesis project about building a website to keep track of children's activities. The application was built using MongoDB, ExpressJS, React, NodeJS.",
+    stack: [
+      { name: "MongoDB" },
+      { name: "ExpressJS" },
+      { name: "React" },
+      { name: "NodeJS" },
+    ],
+    image: "/assets/work/dailytrack.png",
+    github: "https://github.com/hung1503/dailytrack",
+  },
+];
+
+const Work = () => {
+  const [project, setProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="min-h-[80vh] flex flex-col justity-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              {/* Button */}
+              <div className="flex items-center gap-4">
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <span>View code base</span>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+
+                {/* Live project button */}
+                {/* 
+                <Link href={project.github}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <span>View code base</span>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                  */}
+              </div>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidePerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black"></div>
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          atl=""
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Work;
