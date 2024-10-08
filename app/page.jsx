b4682458e24654f4d89f0dbb38692177d23a627c
hung1503 <hungnguyen150300@gmail.com>
2024-10-08 15:30:05 +0300

    Adding Resume file to portfolio

--- app/page.jsx
+++ app/page.jsx
@@ -21,14 +21,16 @@ const Home = () => {
               various programming languages and technologies{" "}
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
-              <Button
-                variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-2"
-              >
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
+              <a href="/assets/resume/Hung_Nguyen_Resume.pdf" download>
+                <Button
+                  variant="outline"
+                  size="lg"
+                  className="uppercase flex items-center gap-2"
+                >
+                  <span>Download CV</span>
+                  <FiDownload className="text-xl" />
+                </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social
                   containerStyles="flex gap-6"
