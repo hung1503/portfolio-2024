b2569a310a8ab66544808df77ce03492765aec54
hung1503 <hungnguyen150300@gmail.com>
2025-10-21 14:57:46 +0300

    Adding live site

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -146,7 +146,7 @@ const Work = () => {
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
                       </TooltipTrigger>
                       <TooltipContent>
                         <span>View project</span>
